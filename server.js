03f0e7bc36609b8280fcf4fa0c85a21f4c183fb6
18subrato <subratoverma7@gmail.com>
2025-08-11 12:16:09 +0530

    added frontend url

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4001
 await connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ["http://localhost:5173","https://quick-auth-frontend.vercel.app"]
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials: true}));
